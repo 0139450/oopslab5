468f330438d11524baed8e548ad03b77724ee7b9
0139450 <159297417+0139450@users.noreply.github.com>
2024-02-29 11:55:29 +0530

    Create typeinher.cpp

---
+++ typeinher.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+/*//single inheritance
+class vehicle{
+    public:
+    void wheeler(){
+        cout<<"the vehicle is a four wheeler"<<endl;
+    }
+    void typemodel(){
+        cout<<"old model"<<endl;
+    }
+};
+//multiple
+class enginee{
+    public:
+    void typeengine(){
+        cout<<"its a petrol vehicle"<<endl;
+    }
+};
+class autos:public vehicle, public enginee{
+    public:
+    void color(){
+        cout<<"auto color is yellow"<<endl;
+    }
+    void modelyr(){
+        cout<<model<<endl;
+
+    }
+    private:
+    int model=99;
+    
+};
+//multilevel inheritance
+//hybrid inheritance
+class rickshaw:public autos{ 
+    public:
+    void wheels(){
+        cout<<"sometimes 3 wheeler"<<endl;
+    }
+
+};
+int main()
+{
+    autos obj;
+    obj.wheeler();
+    obj.typemodel();
+    obj.color();
+    obj.modelyr();
+    obj.typeengine();
+    rickshaw obj1;
+    obj1.wheeler();
+    obj1.typemodel();
+    obj1.color();
+    obj1.modelyr();
+    obj1.wheels();
+}*/
+class vehicle{
+    public:
+    
+};
+
